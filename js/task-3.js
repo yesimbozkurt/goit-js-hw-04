826abb1bd9d5e51594d37efa3b642f011a30eb0f
yesimbozkurt <yesimsbozkurt@gmail.com>
2025-01-29 19:52:33 +0300

    finish

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,21 @@
-
-function filterArray(numbers, value) {
-  const newArray = [];
-
-  for (const number of numbers) {
-    if (number > value) {
-      newArray.push(number);
-    }
+const profile = {
+	username: "Jacob",
+  playTime: 300,
+  getInfo: function() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+  changeUsername: function(newName) {
+    this.username = newName;
+  },
+  updatePlayTime: function(hours) {
+    this.playTime += hours;
   }
-  
-  return newArray;
-}
+};
 
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
