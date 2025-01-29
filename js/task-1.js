85fbbd54d13f09ca12a1e319e548ea1e8aa562d8
Yeşim Bozkurt <yesimsbozkurt@gmail.com>
2025-01-29 17:01:16 +0300

    Initial commit

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+function slugify(title) {
+  let slug = title.toLowerCase()
+  slug = slug.split(" ")
+  slug = slug.join("-") 
+  return slug;
+}
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
